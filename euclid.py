e956000cedfd45cf524efe65f177600f7b1e04e6
GizemAyas517 <gayas14@ku.edu.tr>
2018-05-30 20:44:17 +0300

    n squared version

--- euclid.py
+++ euclid.py
@@ -2,73 +2,62 @@ import math
 
 
 class Point:
-
     def __init__(self, points, line):
         self.points = points
         self.line = line
 
 
 "insert file name here"
-file_name = "sample_input_2_8.tsv"
+file_name = "sample_input_100_100.tsv"
 
-f = open(file_name,"r")
+f = open(file_name, "r")
 
 line = f.readline()
 words = line.split()
 dimension = words.__len__()
 
 
-"close the file to get the dimension first"
 f.close()
 
-f = open(file_name,"r")
+f = open(file_name, "r")
 
-"get the points array"
 
-points=list()
-line_count = 0
+
+points = list()
+line_count = 1
 for line in f:
-    "['-262972', '508697']  --> burda su halder"
     current_point = line.split()
     points.append(Point(points=current_point, line=line_count))
-    line_count+=1
+    line_count += 1
 
 
-def basic_euclidian(point_one, point_two):
-    "sqrt(sum(distances of xs and ys and etc)))"
-    "works on floating or integer"
-    sum=0
-    for index in range(dimension):
-        sum += math.pow(point_one[index]-point_two[index],2)
+
+
+def distance_between(point_one, point_two):
+    """
+
+    :param point_one: multidimensional point, in array format
+    :param point_two: multidimensional point, in array format
+    :return: distance between point one and point two
+    """
+    sum = 0
+    for d1,d2 in zip(point_one,point_two):
+        sum += math.pow(float(d1) - float(d2), 2)
 
     return math.sqrt(sum)
 
 
-def no_optimization_helper(points_array, start_point):
-    "every point will go through here"
-    start_euclidian = basic_euclidian(start_point, points_array[0].points)
-    closest_points = []
-    for point in points_array:
-        euclidian = basic_euclidian(start_point,point.points)
-        if euclidian < start_euclidian:
-            start_euclidian = euclidian
-            closest_points = [start_point, point]
+def find_closest_points():
+    closest_dist = float("inf")
+    closest_points = None, None
+    for y, point_one in enumerate(points):
+        for x, point_two in enumerate(points):
+            if x > y:
+                dist= distance_between(point_one.points,point_two.points)
+                if dist < closest_dist:
+                    closest_dist = dist
+                    closest_points= point_one.line, point_two.line
 
-    return closest_points, start_euclidian
+    return closest_points, point_one.points, point_two.points
 
-"we need to have current smallest euclidian"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print find_closest_points()[0]
